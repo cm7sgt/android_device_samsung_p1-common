54c09ce726a44804dcb89fed1cf4d564b247e0fc
technomancer <dominikbehr@yahoo.com>
2011-10-06 02:25:37 -0700

    add missing file
    Change-Id: I0b365fed4aa6ae468305c835c5081faae5a032c6

---
+++ common.mk
@@ -0,0 +1,85 @@
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=200 \
+    ro.sf.hwrotation=90 \
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=20 \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.setupwizard.enable_bypass=1
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapsize=48m
+
+# use /data/dalvik-cache for system dalvik cache since /cache is only 30MB
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-data-only=1
+
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
+
+# For GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.gps.soket = /data/gpspipe
+
+# Galaxy Tab uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+# For RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+    phone.ril.classname = com.android.internal.telephony.SamsungRIL \
+    ro.telephony.ril_class=samsung
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/p1-common/overlay
+
+# These are the OpenMAX IL configuration files
+PRODUCT_COPY_FILES += \
+    device/samsung/aries-common/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
+    device/samsung/p1-common/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml
+
+# These are the OpenMAX IL modules
+PRODUCT_PACKAGES += \
+    libSEC_OMX_Core.aries \
+    libOMX.SEC.AVC.Decoder.aries \
+    libOMX.SEC.M4V.Decoder.aries \
+    libOMX.SEC.M4V.Encoder.aries \
+    libOMX.SEC.AVC.Encoder.aries \
+    libstagefrighthw
+
+PRODUCT_PACKAGES += \
+    setup_fs \
+    libaudio
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+
+PRODUCT_COPY_FILES += \
+    device/samsung/p1-common/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/p1-common/prebuilt/usr/keylayout/p1_keyboard.kl:system/usr/keylayout/p1_keyboard.kl \
+    device/samsung/p1-common/prebuilt/usr/keylayout/AT42QT602240_Touchscreen.kl:system/usr/keylayout/AT42QT602240_Touchscreen.kl
+
+PRODUCT_COPY_FILES += \
+    device/samsung/p1-common/prebuilt/mke2fs:utilities/mke2fs
+
